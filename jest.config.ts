7a715bf304906049ac04f5e6c85d20c4cceef478
Jason O'Neal <jason.allen.oneal@gmail.com>
2026-03-02 16:50:36 -0500

    ci: fix jest/tsconfig and add smoke test (#10)

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json']
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
 };
+
 export default config;
